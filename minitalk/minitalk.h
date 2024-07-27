c27a4cda05a853b8169095b738223f23547e9545
Redabouizer <150621786+Redabouizer@users.noreply.github.com>
2024-07-27 22:15:50 +0100

    Delete minitalk directory

--- minitalk/minitalk.h
+++
@@ -1,9 +0,0 @@
-#ifndef MINITALK_H
-# define MINITALK_H
-
-#include "utils/utils.h"
-#include <unistd.h>
-# include <limits.h>
-# include <signal.h>
-
-#endif
--- minitalk/utils/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILS_H
-# define UTILS_H
-
-long	ft_atoi(const char *str);
-
-#endif
