17b47502273bd9034a524428e8b5ebd96509a1cf
Redabouizer <150621786+Redabouizer@users.noreply.github.com>
2024-07-27 22:15:59 +0100

    Add files via upload

---
+++ minitalk/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbouizer <rbouizer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/25 15:00:08 by rbouizer          #+#    #+#             */
+/*   Updated: 2024/07/25 16:09:14 by rbouizer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "utils/utils.h"
+# include <unistd.h>
+# include <limits.h>
+# include <signal.h>
+
+#endif
